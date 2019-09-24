772636780c3f0a8470552f12563a09da52562729
unksoc <unksoc@me.com>
2019-09-24 00:18:14 -0500

    Completed Module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,19 +22,19 @@ namespace GradeBook.GradeBooks
             {
                 return 'A';
             }
-            else if (grades[threshold * 2] <= averageGrade)
+            else if (grades[threshold * 2] - 1 <= averageGrade)
             {
                 return 'B';
             }
-            else if (grades[threshold * 3] <= averageGrade)
+            else if (grades[threshold * 3] - 1 <= averageGrade)
             {
                 return 'C';
             }
-            else if (grades[threshold * 4] <= averageGrade)
+            else if (grades[threshold * 4] - 1 <= averageGrade)
             {
                 return 'D';
             }
-            else if (grades[threshold * 5] <= averageGrade)
+            else if (grades[threshold * 5] - 1 <= averageGrade)
             {
                 return 'E';
             }

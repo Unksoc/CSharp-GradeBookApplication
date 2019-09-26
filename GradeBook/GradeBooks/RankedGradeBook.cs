171d115cd02fa0edd719b5b0fd211fad680dc096
unksoc <unksoc@me.com>
2019-09-25 21:14:00 -0500

    Completed Module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,25 +18,29 @@ namespace GradeBook.GradeBooks
             }
             var threshold = (int)Math.Ceiling(Students.Count * .20);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-            switch (averageGrade)
+            if (grades[threshold - 1] <= averageGrade)
             {
-                case var grade when grades[threshold - 1] <= averageGrade:
-                    return 'A';
-
-                case var grade when grades[threshold * 2 - 1] <= averageGrade:
-                    return 'B';
-
-                case var grade when grades[threshold * 3 - 1] <= averageGrade:
-                    return 'C';
-
-                case var grade when grades[threshold * 4 - 1] <= averageGrade:
-                    return 'D';
-
-                case var grade when grades[threshold * 5 - 1] <= averageGrade:
-                    return 'E';
-
-                default:
-                    return 'F';
+                return 'A';
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else if (grades[(threshold * 5) - 1] <= averageGrade)
+            {
+                return 'E';
+            }
+            else
+            {
+                return 'F';
             }
         }
     }
---
+++ GradeBookTests/RankedGradeBookTest.cs
@@ -0,0 +1,38 @@
+﻿using GradeBook.GradeBooks;
+using Xunit;
+
+namespace GradeBookTests
+{
+    public class RankedGradeBookTest
+    {
+        [Fact]
+        public void GetLetterGrade()
+        {
+            RankedGradeBook book = new RankedGradeBook("Book");
+            book.AddStudent(new GradeBook.Student("John", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John1", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John2", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John3", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John4", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John5", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John6", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John7", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John8", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddStudent(new GradeBook.Student("John9", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            //book.AddStudent(new GradeBook.Student("John10", GradeBook.Enums.StudentType.Standard, GradeBook.Enums.EnrollmentType.National));
+            book.AddGrade("John", 25);
+            book.AddGrade("John1", 29);
+            book.AddGrade("John2", 50);
+            book.AddGrade("John3", 10);
+            book.AddGrade("John4", 11);
+            book.AddGrade("John5", 21);
+            book.AddGrade("John6", 21);
+            book.AddGrade("John7", 21);
+            book.AddGrade("John8", 21);
+            book.AddGrade("John9", 21);
+            book.AddGrade("John9", 21);
+            ;
+            Assert.Equal("F", book.GetLetterGrade(3).ToString());
+        }
+    }
+}
